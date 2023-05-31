794fb80cf05217f499db0bdad80f6b2e14b72710
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-05-31 14:17:19 +0000

    CPT-02

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* Global Phaser */
 
+// Copyright (c) 2023 Hewett All rights reserved
+//
+// Created by: Santiago Hewett
+// Created on: May 2023
+// This is the phaser3 configuration file
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // Set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // We place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
